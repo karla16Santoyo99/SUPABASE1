2093345dd525148f4e2c3dd8f3b9c351fb113e28
karla16Santoyo99 <64182574+karla16Santoyo99@users.noreply.github.com>
2022-03-01 00:06:49 -0600

    cambios en el env

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ohmjxqthjlmvctulxyux.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9obWp4cXRoamxtdmN0dWx4eXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDU0OTcwOTksImV4cCI6MTk2MTA3MzA5OX0.s6E3x5ow_Vd042sF0imxYkhHo8Q9hlk8mZqU2d9mMNo"
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
